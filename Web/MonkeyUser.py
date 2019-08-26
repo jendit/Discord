1bbbf34ad28b22549d5cfb68463745773cfa96ad
jendit <github@jendit.de>
2019-08-26 15:39:23 +0200

    Setup development and create base module for monkeyuser.

---
+++ Web/MonkeyUser.py
@@ -0,0 +1,14 @@
+#! python3
+# MonkeyUser.py - get comic issues from monkeyuser.com
+
+import requests
+import bs4
+
+
+class MonkeyUser:
+    URL = 'https://www.monkeyuser.com/'
+
+
+    #TODO get latest comic
+
+    #TODO get random comic
